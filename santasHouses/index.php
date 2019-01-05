5eeb945ab59c979aa082caef3f679fd89ac36f1c
HirOta-kun <kis.zoltan.cs@gmail.com>
2019-01-05 14:55:49 +0100

    added part two,but for some reason it doesn't give the good answer in the end-yet!

--- santasHouses/index.php
+++ santasHouses/index.php
@@ -13,29 +13,77 @@
         include 'data.php';
 
         $arr = str_split($str);
+        //House they visited at least once
         $house = array(0, 0);  //begining house
-        $houses = array($house);  //the houses what santa visited
-        $x = 0;
-        $y = 0;
+        $houses = array($house);  //the houses what get present
+        //Santa's coordinate
+        $sX = 0; 
+        $sY = 0;
+        //RoboSanta's coordinate
+        $rX = 0;
+        $rY = 0;
+        $counter = 0;
         //use the direction as a two-axis coordinate system
+        //part one
          foreach( $arr as $coor){
            //get the x coordinate
            if ($coor == '>'){
-            $x++;
+            $sX++;
            }else if($coor == '<'){
-            $x--; 
+            $sX--; 
            }else if ($coor == '^'){
            //get the y coordinate
-             $y++;
+             $sY++;
            }else if ($coor == 'v'){
-             $y--;
+             $sY--;
            }
-           $house = array($x, $y);  //fix it in an array
+           $house = array($sX, $sY);  //fix it in an array
            if(!in_array($house, $houses)){  //check that santa was it here
              array_push($houses, $house);   //if not, push it to the main array
            }
           };
-          print 'The Santa visited ' . count($houses) . ' houses at least once!';
+          print 'The Santa visited ' . count($houses) . ' houses at least once! <br>';
+
+          //part two
+          $house = array(0, 0);  //begining house
+          $houses = array($house);  //the houses what get present
+          foreach( $arr as $coor){
+            if ($counter % 2 == 0){
+              //get the x coordinate
+              if ($coor == '>'){
+              $sX++;
+              }else if($coor == '<'){
+              $sX--; 
+              }else if ($coor == '^'){
+              //get the y coordinate
+                $sY++;
+              }else if ($coor == 'v'){
+                $sY--;
+              }
+              $house = array($sX, $sY);  //fix it in an array
+              if(!in_array($house, $houses)){  //check that santa was it here
+                array_push($houses, $house);   //if not, push it to the main array
+              }
+            }else{
+              //get the x coordinate
+              if ($coor == '>'){
+                $rX++;
+                }else if($coor == '<'){
+                $rX--; 
+                }else if ($coor == '^'){
+                //get the y coordinate
+                  $rY++;
+                }else if ($coor == 'v'){
+                  $rY--;
+                }
+                $house = array($rX, $rY);  //fix it in an array
+                if(!in_array($house, $houses)){  //check that RoboSanta was it here
+                  array_push($houses, $house);   //if not, push it to the main array
+                }
+            }
+            $counter++;
+           };
+           print 'In the next year the Santa and the RoboSanta visited ' . count($houses) . ' houses at least once!';
       ?>
   </body>
 
